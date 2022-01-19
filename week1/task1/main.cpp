9db7f8da4be9d1b7fe0c698cfab30dd43d01e197
Bersen66 <orturB@yandex.ru>
2022-01-19 19:35:22 +0300

    initial commit

---
+++ week1/task1/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by ortur on 19.01.2022.
+//
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world! 1" << std::endl;
+    return 0;
+}
---
+++ week1/task2/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by ortur on 19.01.2022.
+//
+
+
+#include <iostream>
+#include "test_runner.h"
+#include "profile.h"
+
+int main() {
+    std::cout << "Hello, world! 2" << std::endl;
+    return 0;
+}
